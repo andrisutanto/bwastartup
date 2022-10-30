a588da952979fcc39f8add52fd6a043032606fc2
Andri Sutanto <me@andrisutanto.com>
2022-10-30 09:26:20 +0700

    Testing JSON Response
    Show JSON response when access from client

--- main.go
+++ main.go
@@ -1,42 +1,63 @@
 package main
 
 import (
-	"bwastartup/user"
-	"fmt"
 	"log"
+	"os/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// } else {
+	// 	fmt.Println("Successfully connected!")
+	// }
+
+	// //menampilkan data dari DB
+	// //nama plural, karena jamak
+	// var users []user.User
+
+	// /*
+	// 	length := len(users)
+	// 	fmt.Println(length)
+	// 	length = len(users)
+	// 	fmt.Println(length)
+	// */
+
+	// //tipe pointer, mencari data di table users
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("============")
+	// }
+
+	// buat router di GIN
+	router := gin.Default()
+	// saat mengakses /handler, akan diproses oleh func handler
+	router.GET("/handler", handler)
+	// untuk menjalankan routernya
+	router.Run()
+
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
-	} else {
-		fmt.Println("Successfully connected!")
 	}
 
-	//menampilkan data dari DB
-	//nama plural, karena jamak
 	var users []user.User
-
-	/*
-		length := len(users)
-		fmt.Println(length)
-		length = len(users)
-		fmt.Println(length)
-	*/
-
-	//tipe pointer, mencari data di table users
 	db.Find(&users)
 
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("============")
-	}
-
+	c.JSON(200, users)
 }
