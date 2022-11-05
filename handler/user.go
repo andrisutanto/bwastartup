cf366e2311bdd9f57c1f69ef8b53ac2ce178618e
Andri Sutanto <me@andrisutanto.com>
2022-11-06 06:59:23 +0700

    Upload Avatar: Fix Filename
    Fix filename for avatar upload

--- handler/user.go
+++ handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bwastartup/helper"
 	"bwastartup/user"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -162,7 +163,12 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	path := "images/" + file.Filename
+	//anggap ID nya 1, seharusnya dapat dari JWT
+	userID := 1
+
+	//tambahkan user ID pada filename agar tidak kembar
+	//path := "images/" + file.Filename
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
 	err = c.SaveUploadedFile(file, path)
 	if err != nil {
@@ -172,9 +178,6 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-
-	//anggap ID nya 1, seharusnya dapat dari JWT
-	userID := 1
 
 	_, err = h.userService.SaveAvatar(userID, path)
 	if err != nil {
