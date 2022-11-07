8c90b49d1e156c3b028084fcd1cc8c065ead45f3
Andri Sutanto <me@andrisutanto.com>
2022-11-07 21:42:51 +0700

    Campaign List: Repository
    Create repository for Campaign List (List All Campaigns and Find Campaign by ID)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,7 @@
 package handler
+
+// tangkap parameter di handler
+// handler proses ke service
+// service menentukan, repository mana yang di call
+// repository ada 2, FindAll dan FindByUserID
+// DB
