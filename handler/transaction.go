d26dcf2ef61639fd6ed503943ebb2846fc31313f
Andri Sutanto <me@andrisutanto.com>
2022-12-24 19:30:43 +0800

    Create New Transaction: Handler
    Create Handler for new transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -77,3 +77,34 @@ func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
 //handler tangkap input terus dimapping ke input struct
 //panggil service utk simpan transaksi, panggil snap gateway (midtrans)
 //service panggil repository utk create new transaction data
+
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to create transaction", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	//ini ambil current user dari JWT
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	newTransaction, err := h.service.CreateTransaction(input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	//kalau tidak ada error
+	response := helper.APIResponse("Success to create transaction", http.StatusOK, "success", newTransaction)
+	c.JSON(http.StatusOK, response)
+}
